49aab6bb9b13827db18f74350e1be17e47491b81
Gladys794 <alvarezgarcia93@hotmail.com>
2016-11-19 13:44:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var htto = require('http').server(app);
+//usando socketio
+var io= require('socket.io')(http);
+
+app.use(express.static(__dirname + ''))//aqui se serviran archivos estaticos
+
+var port = process.env.PORT || 3000;//esto lo exige heroku
+
+app.get('/',function(req , res){
+	res.sendFile(__dirname + '/index.html');//ruta pag index
+
+});
+
+http.listen(port, function)(){
+	console.log('Escuchando en el puerto: '+port);
+}
