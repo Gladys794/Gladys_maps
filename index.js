6a46210b7b8c4fe4ef95af50aa862e34116d124a
Gladys794 <alvarezgarcia93@hotmail.com>
2016-11-26 10:30:31 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ app.get('/', function(req, res){
 http.listen(port,function(){
     console.log('Escuchando en el puerto: '+port);//mensaje en consola 
 });
+
+var socketCount=0;//contador de conexiones al server
+
+io.on('conexion', function(socket){
+    console.log('Usuario conectado...');
+    socketCount++;
+    //emitiendo mensaje a todos los socket o usuarios conectados
+    io.sockets.emit('usuario conectado', socketCount);
+});//cierra conexion
+
+socket.on('disconnect',function(){
+	socketCount--;//decremento del contador
+	console.log('Usuario desconectado');//mensaje en consola
+	io.sockets.emit('Usuario desconectado');//mensaje a todos los usuarios
+});
